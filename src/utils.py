32f119e2ac4125144f371442e9c14e150fac6692
Carlos Alves <cmalves@users.noreply.github.com>
2022-05-05 10:53:09 +0100

    Workshop exercise (#10)
    * docs: model deployment
    
    * docs: improve text
    
    * feat: inference endpoint
    
    * chore: update lock
    
    * feat: inference endpoint
    
    * chore: install packages
    
    * chore: fix import
    
    * chore: update inference
    
    * Update readme (#6)
    
    * chore: create requirements
    
    * docs: add project setup and introduction
    
    * docs: update documentation
    
    * docs: add model deployment info
    
    * Wdl workshop improvements (#8)
    
    * WDL Workshop (#7)
    
    * docs: model deployment
    
    * docs: improve text
    
    * feat: inference endpoint
    
    * chore: update lock
    
    * feat: inference endpoint
    
    * chore: install packages
    
    * chore: fix import
    
    * chore: update inference
    
    * Update readme (#6)
    
    * chore: create requirements
    
    * docs: add project setup and introduction
    
    * docs: update documentation
    
    * docs: add model deployment info
    
    * chore: inference wdl workshop
    
    * chore: remove preprocessing
    
    * chore: update requirements
    
    * chore: bump nlp training version
    
    * chore:
    
    * chore:
    
    * Add ml files to Dockerfile (#9)
    
    * chore: exercise

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,8 @@ from inference.data_processors.transformers.preprocessing import (
     VocabTransform
 )
 
+from inference.data_processors.transformers.postprocessing.get_max import GetMaxPrediction
+
 
 # Constants
 MODEL_PATH = "ml/model.ckpt"
@@ -50,17 +52,17 @@ def get_preprocessor() -> Processor:
     Processor
         Preprocessing pipeline steps to be applied before inference.
     """
-    preprocessing = [
-        NLPiperIntegration(
-            pipeline=nlpiper.core.Compose(
-                [
-                    nlpiper.transformers.cleaners.CleanPunctuation(),
-                    nlpiper.transformers.tokenizers.BasicTokenizer(),
-                    nlpiper.transformers.normalizers.CaseTokens(),
-                ]
-            )
-        ),
-        VOCAB_TRANSFORM
-    ]
+    preprocessing = []  # TODO: Implement preprocessing pipeline
 
     return Processor(preprocessing=preprocessing)
+
+
+def get_postprocessor() -> Processor:
+    """Load postprocessing pipeline.
+
+    Returns
+    -------
+    Processor
+        Postprocessing pipeline steps to be applied after inference.
+    """
+    # TODO: Implement postprocessing pipeline
